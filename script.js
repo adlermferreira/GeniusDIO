53dfc290a6c751ab98ef4d36e470dada619d5dad
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-10-22 19:54:50 -0300

    genius style and color order

---
+++ script.js
@@ -0,0 +1,17 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 green 1 red 2 yellow 3 blue
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+let shuffleOrder = () => {
+	let colorOrder = Math.floor(Math.random() * 4);
+	order[order.length] = colorOrder;
+	clickedOrder = [];
+
+}
