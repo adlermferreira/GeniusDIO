e61cdf7410b67005bff5f4d7d7c9faa2cef0ab9e
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-10-22 20:24:08 -0300

    done

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let clickedOrder = [];
 let score = 0;
 
 // 0 green 1 red 2 yellow 3 blue
-
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
 const green = document.querySelector('.green');
+const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
 
 let shuffleOrder = () => {
 	let colorOrder = Math.floor(Math.random() * 4);
@@ -33,7 +32,7 @@ let lightColor = (element, number) => {
 let checkOrder = () => {
 	for(let i in clickedOrder) {
 		if(clickedOrder[i] !== order[i]) {
-			lose();
+			gameOver();
 			break;
 		}
 	}
@@ -47,9 +46,42 @@ let click = (color) => {
 	clickedOrder[clickedOrder.length] = color;
 	createColorElement(color).classList.add('selected');
 
-	setTimeout(() = > {
+	setTimeout(() => {
 		createColorElement(color).classList.remove('selected');		
-	})
-
-	checkOrder();
+		checkOrder();
+	}, 250);
 }
+
+let createColorElement = (color) => {
+	if(color === 0) return green;
+	else if (color === 1) return red;
+	else if (color === 2) return yellow;
+	else if (color === 3) return blue;
+}
+
+let nextLevel = () => {
+	score++;
+	shuffleOrder();
+}
+
+let gameOver = () => {
+	alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+	order = [];
+	clickedOrder = [];
+
+	playGame();
+}
+
+let playGame = () => {
+	alert(`Bem video ao Gênesis! Iniciando novo jogo!`);
+	score = 0;
+
+	nextLevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
